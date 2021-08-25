c7df83ead9f2a06786983b46bcc3132aea22c27d
IKENNA <ikenna@IKENNAs-MacBook-Pro.local>
2021-08-25 06:51:19 +0100

    First commit

---
+++ lib/post_view.dart
@@ -0,0 +1,29 @@
+import 'package:bloc_and_cubit/post.dart';
+import 'package:bloc_and_cubit/posts_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class PostView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Posts'),
+      ),
+      body: BlocBuilder<PostsCubit, List<Post>>(builder: (context, posts) {
+        if (posts.isEmpty) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return ListView.builder(itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(posts[index].title),
+            ),
+          );
+        });
+      }),
+    );
+  }
+}
---
+++ lib/posts_cubit.dart
@@ -0,0 +1,10 @@
+import 'package:bloc_and_cubit/data_services.dart';
+import 'package:bloc_and_cubit/post.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class PostsCubit extends Cubit<List<Post>> {
+  final _dataService = DataService();
+  PostsCubit() : super([]);
+
+  void getPosts() async => emit(await _dataService.getPosts());
+}
